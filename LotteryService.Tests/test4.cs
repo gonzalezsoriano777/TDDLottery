4c1bcd0923fecb82feab8a222a7a97d9347c6190
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2019-09-13 13:19:48 -0500

    Test Four Complete. All 10 numbers are sorted in array.

--- LotteryService.Tests/test4.cs
+++ LotteryService.Tests/test4.cs
@@ -6,10 +6,16 @@ namespace LotteryTest
 {
     class LotteryFour
     {
+        lotteryService lotteryFour = new lotteryService();
+
         [Test]
         public void ReturnTenNumSorted()
         {
-
+            int[] lotteryNumber4 = new int[10] { 70, 40, 60, 80, 160, 4820, 583, 83, 6942, 7 };
+            var output = lotteryFour.SortNumbers(lotteryNumber4);
+            int[] fakeLottery = new int[10] { 58382, 5839, 5822, 6324, 5722, 5932, 942, 5, 6, 2 };
+            var secondOuput = lotteryFour.SortNumbers(fakeLottery);
+            Assert.AreEqual(lotteryNumber4, output);
         }
     }
 }
